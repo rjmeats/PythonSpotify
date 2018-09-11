11d3ab473d702d09543968f41f3f7b6cc8243f11
Richard Meats <35453720+rjmeats@users.noreply.github.com>
2018-09-11 11:31:37 +0100

    Rework file reading

--- PythonSpotipy/MyPlaylists.py
+++ PythonSpotipy/MyPlaylists.py
@@ -2,41 +2,39 @@ import spotipy
 import spotipy.util as util
 import os
 
-token=""
-try:
-    with open('token.txt') as f:
-        token = f.read()
-    f.close
-except :
-    print("Failure to read token file")
 
-if not token:
-    print("No token read")
-    exit()
+env = {}
 
-userid=""
-try:
-    with open('userid.txt') as f:
-        userid = f.read()
-    f.close
-except :
-    print("Failure to read user ID file")
+# Read a value from an external file, and store it in the global env map.
+def setFromFile(label):
+    fileValue = ""
+    fileName = label + ".txt"
+    try:
+        with open(fileName) as f:
+            fileValue = f.read()
+        f.close
+    except :
+        print("Failure reading " + label + " file: " + fileName)
 
-if not userid:
-    print("No userid read")
-    exit()
-
-sp = spotipy.Spotify(auth=token)
+    if fileValue:
+        env[label] = fileValue
+    else :
+        print("No " + label + " value read")
+        exit()
 
 def showTracks(userid, playlistId):
     playlistTracks = sp.user_playlist(userid, playlistId)
     for t, track in enumerate(playlistTracks['tracks']['items']) :
         print("  %4d  %s" % (t+1, track['track']['name']))
 
+setFromFile('token')
+setFromFile('userid')
+
+sp = spotipy.Spotify(auth=env['token'])
+
 playlists = sp.current_user_playlists()
 
 for i, playlist in enumerate(playlists['items']):
-    #print(playlist)
     print("%4d %s %s %s" % (i + 1 + playlists['offset'], playlist['owner']['display_name'],  playlist['id'],  playlist['name']))
-    showTracks(userid, playlist['id'])
+    showTracks(env['userid'], playlist['id'])
 
